5ecfbf92b71aa54220a3e462645fee1867c24d8a
Saurav Yadav <71329650+WizzCodes@users.noreply.github.com>
2021-05-20 12:45:17 +0530

    Add files via upload

---
+++ Tut7.py
@@ -0,0 +1,19 @@
+#Var1 = 55
+#Var2 = 23
+#Var3 = int(input())
+
+#if Var3>Var1:
+ #   print("Greater")
+  #  elif Var3==Var1:
+   #     print("Equal")
+#else:
+ #   print("Lesser")
+
+
+
+list1 = [2, 5, 3]
+print(7 in list1)
+if 7 in list1:
+    print("Yes its in the list1")
+else:
+    print("No its not in the list")
